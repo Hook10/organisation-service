51773c53d05358cbe23510f10d3338286fb6ef81
AskarSap <sapasask@gmail.com>
2023-06-03 06:29:18 +0300

    feature_organizationMapper

--- src/main/java/com/hook/organisation/dto/OrganizationDto.java
+++ src/main/java/com/hook/organisation/dto/OrganizationDto.java
@@ -1,5 +1,21 @@
 package com.hook.organisation.dto;
 
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrganizationDto {
+
+  private Long id;
+  private String organizationName;
+  private String organizationDescription;
+  private String organizationCode;
+  private LocalDateTime createDate;
 
 }
